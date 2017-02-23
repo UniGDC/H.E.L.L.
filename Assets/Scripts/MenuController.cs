a46578b5e121e58dcafeeea7f1a371dfe6734dfd
syy1125 <syy1125@hotmail.com>
2017-02-23 08:51:03 -0800

    Pause/Resume button
    Added a pause/resume button.

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    public Text ButtonText;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void TogglePause()
+    {
+        // Just an arbitrarily decided boundary.
+        if (Time.timeScale <= 1E-5)
+        {
+            Time.timeScale = 1;
+            ButtonText.text = "Pause";
+        }
+        else
+        {
+            Time.timeScale = 0;
+            ButtonText.text = "Resume";
+        }
+    }
+}
